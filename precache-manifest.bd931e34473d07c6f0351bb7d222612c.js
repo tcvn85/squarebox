ea61939a2f7246623db1db08eeeb249c61987c8d
TrungN <tcvn85@gmail.com>
2020-10-01 00:41:38 +0700

    Updates

---
+++ precache-manifest.bd931e34473d07c6f0351bb7d222612c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1240889111deb83fbb81cdf31575bcf",
+    "url": "/squarebox/index.html"
+  },
+  {
+    "revision": "2b8cab52f6d74c9854bf",
+    "url": "/squarebox/static/css/main.3d655d53.chunk.css"
+  },
+  {
+    "revision": "0c93cb5f7a453b53b721",
+    "url": "/squarebox/static/js/2.ab5eb57c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/squarebox/static/js/2.ab5eb57c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8cab52f6d74c9854bf",
+    "url": "/squarebox/static/js/main.5061be65.chunk.js"
+  },
+  {
+    "revision": "ac87525f08d9c1d492e4",
+    "url": "/squarebox/static/js/runtime-main.4a0110f8.js"
+  }
+]);
